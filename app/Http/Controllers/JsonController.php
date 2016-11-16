6e17aad9fefcdca47069300096e278ba96aa8b49
anggarasaja <harrisanggara@gmail.com>
2016-11-16 20:39:38 +0700

    updater add DB facade

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\JsonModel;
-
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests;
 
 class JsonController extends Controller
